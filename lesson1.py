7234dadb2d3e0250fc99de899cd4f7d295c6d3a9
Serhii Volikov <volikovserhii96@gmail.com>
2022-02-03 12:19:24 +0200

    1

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("Hello", "World")
+print("Hello " + "World")
+print("Hello", "World", sep=" ")
+print("Hello", "World", sep="\t")
+print("Hello", end=" World\n")
+print("13+10", end="=")
+print("23")
+
+#for PyCharm
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n#\t\t#\n#\t\t#\n#########\t\t\n#\t\t#\n#\t\t#")
+print("#########","#\t\t#","# \t\t#","#\t\t#","#########\n","#\t\t#","#\t\t#","#########","#\t\t#","#\t\t#", sep="\n")
+#for terminal
+print("#########","#\t#","# \t#","#\t#","#########\n","#\t#","#\t#","#########","#\t#","#\t#", sep="\n")
+print("#########\n#\t#\n#\t#\n#\t#\n#########\n\n#\t#\n#\t#\n#########\t\n#\t#\n#\t#")
