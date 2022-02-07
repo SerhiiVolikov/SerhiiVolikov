eb2f91d7f4fc69d3a103d5d74af412ff3a3d9379
Serhii Volikov <volikovserhii96@gmail.com>
2022-02-07 14:51:11 +0200

    2

--- Lesson2/Task 2.py
+++ Lesson2/Task 2.py
@@ -1,4 +1,4 @@
-print("       VALID NUMBER TEST")
+print("  UKRANIAN NUMBER VALID TEST")
 phone_number = input("Please enter your phone number:\n")
 number_length = 12
 Ukraine_phone_code = "380"
@@ -10,9 +10,9 @@ else:
 if phone_number.isdecimal():
     pass
 else:
-     raise SystemExit("Number is invalid")
+    raise SystemExit("Number is invalid")
 if phone_number[0:3] == Ukraine_phone_code:
     print("Numer is valid")
 else:
-     raise SystemExit("Number is invalid")
+    raise SystemExit("Number is invalid")
 
