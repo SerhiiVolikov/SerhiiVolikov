58723f85d8a92f19214befe0be4da1071a516e6b
Serhii Volikov <volikovserhii96@gmail.com>
2022-02-12 00:49:46 +0200

    1

--- Lesson4/Task 4.py
+++ Lesson4/Task 4.py
@@ -1,10 +1,13 @@
-print("Insert your expression: "), input()
+print("Insert your expression: ")
+expression = input()
 answer = int(input("The answer will be: "))
-print("Lets check")
-expression = input('Insert mathematical expression: ')
-print(("Will be:"), eval(expression))
+print("I need to check it"), (eval(expression))
 while answer != (eval(expression)):
     print("You're wrong, try again: ")
-    answer = int(input("The answer will be: "))
+    if answer != (eval(expression)):
+        answer = int(input("Will be: "))
+        print("You're wrong")
+        print("Will be:", eval(expression))
+        break
 else:
-    print("You're right ")
+    print("You are right.")
