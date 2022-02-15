2a13ff9c8de239c8a3aeb31454fe7a0489e31590
Serhii Volikov <volikovserhii96@gmail.com>
2022-02-15 15:01:04 +0200

    2

--- Homework/Lesson 6/Task 3.py
+++ Homework/Lesson 6/Task 3.py
@@ -1,7 +1,5 @@
 
-list_1 = [i for i in range(1, 11)]
-print(list_1)
+list_1 = [i for i in range(1,11)]
 list_2 = [number ** 2 for number in list_1]
-print(list_2)
 result = tuple(zip(list_1,list_2))
 print(result)
